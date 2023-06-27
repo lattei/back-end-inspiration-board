7a295c9443259d37347cc6bda856915c0a250b22
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-06-27 08:07:19 -0600

    fixed the boards routes

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,65 @@
 from flask import Blueprint, jsonify, request
-from app.models import Card
+from app.models import Board, Card
 from app import db
 
+board_bp = Blueprint('board', __name__, url_prefix='/boards')
 card_bp = Blueprint('card', __name__, url_prefix='/cards')
+
+@board_bp.route('/', methods=['GET'])
+def get_boards():
+    boards = Board.query.all()
+    board_list = [board.to_json() for board in boards]
+    return jsonify(board_list)
+
+@board_bp.route('/', methods=['POST'])
+def create_board():
+    data = request.json
+    title = data.get('title')
+    owner = data.get('owner')
+
+    board = Board(title=title, owner=owner)
+    db.session.add(board)
+    db.session.commit()
+
+    return jsonify({'message': 'Board created successfully'}), 201
+
+@board_bp.route('/<int:board_id>', methods=['GET'])
+def get_board(board_id):
+    board = Board.query.get_or_404(board_id)
+    return jsonify(board.to_json())
+
+@board_bp.route('/<int:board_id>', methods=['PUT'])
+def update_board(board_id):
+    board = Board.query.get_or_404(board_id)
+    data = request.json
+    title = data.get('title')
+    owner = data.get('owner')
+
+    board.title = title
+    board.owner = owner
+    db.session.commit()
+
+    return jsonify({'message': 'Board updated successfully'})
+
+@board_bp.route('/<int:board_id>', methods=['DELETE'])
+def delete_board(board_id):
+    board = Board.query.get_or_404(board_id)
+    db.session.delete(board)
+    db.session.commit()
+
+    return jsonify({'message': 'Board deleted successfully'})
+
+@board_bp.route('/<int:board_id>/cards', methods=['GET'])
+def get_board_cards(board_id):
+    board = Board.query.get_or_404(board_id)
+    cards = board.cards
+    card_list = [card.to_json() for card in cards]
+    return jsonify(card_list)
 
 @card_bp.route('/', methods=['GET'])
 def get_cards():
     cards = Card.query.all()
-    card_list = []
-    for card in cards:
-        card_list.append({
-            'id': card.id,
-            'message': card.message,
-            'likes_count': card.likes_count
-        })
+    card_list = [card.to_json() for card in cards]
     return jsonify(card_list)
 
 @card_bp.route('/', methods=['POST'])
@@ -31,12 +77,7 @@ def create_card():
 @card_bp.route('/<int:card_id>', methods=['GET'])
 def get_card(card_id):
     card = Card.query.get_or_404(card_id)
-    card_data = {
-        'id': card.id,
-        'message': card.message,
-        'likes_count': card.likes_count
-    }
-    return jsonify(card_data)
+    return jsonify(card.to_json())
 
 @card_bp.route('/<int:card_id>', methods=['PUT'])
 def update_card(card_id):
@@ -58,3 +99,4 @@ def delete_card(card_id):
     db.session.commit()
 
     return jsonify({'message': 'Card deleted successfully'})
+
