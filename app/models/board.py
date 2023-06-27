8f16575903682b86abc9bf2eae412d8d9f8ae66b
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-06-27 07:29:13 -0600

    refactored some routes

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,20 @@
 from app import db
+
+
+class Board(db.Model):
+    __tablename__ = 'board'
+
+    id= db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    owner = db.Column(db.String(255))
+
+    def __init__(self,title, owner):
+        self.title = title
+        self.owner = owner
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "owner": self.owner
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,89 +1,60 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, jsonify, request
+from app.models import Card
 from app import db
 
-class Board:
-    def __init__(self, id, title, owner):
-        self.id = id
-        self.title = title
-        self.owner = owner
+card_bp = Blueprint('card', __name__, url_prefix='/cards')
 
-boards = [
-    Board(1, "Board 1", "Owner 1"),
-    Board(2, "Board 2", "Owner 2"),
-    Board(3, "Board 3", "Owner 3"),
-    Board(4, "Board 4", "Owner 4"),
-
-]
-
-board_bp = Blueprint("board", __name__, url_prefix="/boards")
-
-@board_bp.route("", methods=["GET"])
-def get_all_boards():
-    boards = Board.query.all()
-    boards_response = []
-    for board in boards:
-        boards_response.append({
-            "id": board.board_id,
-            "title": board.title,
-            "owner": board.owner
+@card_bp.route('/', methods=['GET'])
+def get_cards():
+    cards = Card.query.all()
+    card_list = []
+    for card in cards:
+        card_list.append({
+            'id': card.id,
+            'message': card.message,
+            'likes_count': card.likes_count
         })
-    return jsonify(boards_response)
+    return jsonify(card_list)
 
-@board_bp.route("", methods=["GET"])
-def get_board(self, board_id):
-    board = Board.query.get(board_id)
-    return {
-        "id": board.board_id,
-        "title": board.title,
-        "owner": board.owner
+@card_bp.route('/', methods=['POST'])
+def create_card():
+    data = request.json
+    message = data.get('message')
+    likes_count = data.get('likes_count')
+
+    card = Card(message=message, likes_count=likes_count)
+    db.session.add(card)
+    db.session.commit()
+
+    return jsonify({'message': 'Card created successfully'}), 201
+
+@card_bp.route('/<int:card_id>', methods=['GET'])
+def get_card(card_id):
+    card = Card.query.get_or_404(card_id)
+    card_data = {
+        'id': card.id,
+        'message': card.message,
+        'likes_count': card.likes_count
     }
-@board_bp.route("/", methods=["POST"])
-def create_board():
-    request_body = request.get_json()
-    new_board = Board(title=request_body["title"],
-                    owner=request_body["owner"])
-    db.session.add(new_board)
+    return jsonify(card_data)
+
+@card_bp.route('/<int:card_id>', methods=['PUT'])
+def update_card(card_id):
+    card = Card.query.get_or_404(card_id)
+    data = request.json
+    message = data.get('message')
+    likes_count = data.get('likes_count')
+
+    card.message = message
+    card.likes_count = likes_count
     db.session.commit()
-    return make_response(f"Board {new_board.title} successfully created", 201)
 
-@board_bp.route("/<board_id>", methods=["PUT"])
-def update_board(board_id):
-    board = Board.query.get(board_id)
-    form_data = request.get_json()
-    board.title = form_data["title"]
-    board.owner = form_data["owner"]
+    return jsonify({'message': 'Card updated successfully'})
+
+@card_bp.route('/<int:card_id>', methods=['DELETE'])
+def delete_card(card_id):
+    card = Card.query.get_or_404(card_id)
+    db.session.delete(card)
     db.session.commit()
-    return make_response(f"Board #{board.board_id} successfully updated")
 
-@board_bp.route("/<board_id>", methods=["DELETE"])
-def delete_board(board_id):
-    board = Board.query.get(board_id)
-    db.session.delete(board)
-    db.session.commit()
-    return make_response(f"Board #{board.board_id} successfully deleted")
-
-
-class Card:
-    def __init__(self, id, message, board_id):
-        self.id = id
-        self.message = message
-        self.board_id = board_id
-
-cards = [
-    Card(1, "Card 1", 1),
-    Card(2, "Card 2", 1),
-    Card(3, "Card 3", 1),
-    Card(4, "Card 4", 1),
-
-]
-
-card_bp = Blueprint("card", __name__, url_prefix="/cards")
-@card_bp.options("", methods=["OPTIONS"])
-def handle_options():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "*")
-    response.headers.add("Access-Control-Allow-Methods", "*")
-    return response
-
-
+    return jsonify({'message': 'Card deleted successfully'})
