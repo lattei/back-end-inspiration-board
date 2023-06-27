05a9ca7ab1c57c9188a8f050e4d61a6f851302a6
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-06-27 07:34:54 -0600

    moved classes to own files

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,22 @@
 from app import db
+
+
+class Card(db.Model):
+    __tablename__ = 'card'
+
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(255))
+    likes_count = db.Column(db.Integer)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'))
+
+    def __init__(self, message, likes_count, board_id):
+        self.message = message
+        self.likes_count = likes_count
+        self.board_id = board_id
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'message': self.message,
+            'likes_count': self.likes_count
+        }
