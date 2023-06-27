b029b4fdec6017497d7c71fb5e2ca20179826d57
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-06-27 08:07:52 -0600

    boards routes

--- app/__init__.py
+++ app/__init__.py
@@ -29,6 +29,7 @@ def create_app():
     # app.register_blueprint(example_bp)
     from .routes import board_bp
     from .routes import card_bp
+    
     app.register_blueprint(board_bp)
     app.register_blueprint(card_bp)
 
